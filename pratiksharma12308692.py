bb3b4c8122bc3cae3c02affd34830e0f187f975b
pratiksha290 <150111355+pratiksha290@users.noreply.github.com>
2025-04-22 10:46:47 +0530

    Update pratiksharma12308692.py

--- pratiksharma12308692.py
+++ pratiksharma12308692.py
@@ -8,11 +8,7 @@ print(data.shape)
 print(data.head())
 
 # Check and visualize missing values
-sns.heatmap(data.isnull(), cbar=False, cmap='YlGnBu')
-plt.title('Missing Values Heatmap', fontsize=12)
-plt.xticks(rotation=90, fontsize=8)
-plt.yticks(fontsize=8)
-plt.tight_layout()
+sns.heatmap(data.isnull(), cbar=False, cmap='
 plt.show()
 
 # Drop columns with many NaNs
